ad0d4b62e562b45c29c5c945806f3611c4314fc9
omercantasdemir <58983991+omercantasdemir@users.noreply.github.com>
2020-05-19 10:32:02 +0300

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arduino_serial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            string[] io_pins = SerialPort.GetPortNames();
+        
+            serialPort1.PortName = "COM3";
+            serialPort1.Open();
+            serialPort1.Write("b");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("3");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("2");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Write("2");
+        }
+    }
+}
